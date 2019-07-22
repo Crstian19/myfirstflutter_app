177dfef79bf4e10743c0de8bd2d61c0ab980620c
Cristian <kiyoma19@gmail.com>
2019-07-22 19:27:10 +0200

    Tarea list

---
+++ lib/TabPage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+
+class TabPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() =>
+
+  return
+
+  new
+
+  TabPageState();
+
+
+}
+
+class TabPageState extends State<TabPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
